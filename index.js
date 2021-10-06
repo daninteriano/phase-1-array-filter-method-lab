302f42c1f5f34c8af8f5998398c5da28ee387b9f
Daniel Interiano <dannyboyint@gmail.com>
2021-10-06 13:49:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const { object } = require("webidl-conversions");
+
 // Code your solution here
+function findMatching(array, string){
+   const theChosenOnes = array.filter(function(word){
+       return word === string})
+    return theChosenOnes;
+}
+
+
+function matchName(array, string){
+     return array.filter(function(){
+        return object.values(array) === string;
+    })
+}
+
+function fuzzyMatch(array, string){
+    const thoseMatched = array.filter(function(word){
+        return word.includes(string); 
+       })
+    return thoseMatched;
+}
